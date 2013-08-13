63ad7fd40d07a503f55a7b99757a12950f4ddf3a
jplouis <jplouis@gmail.com>
2013-08-13 09:59:21 -0500

    clean up lua code and add inline comments

--- conf/zope-hipache.lua
+++ conf/zope-hipache.lua
@@ -1,7 +1,7 @@
     -- Connect to Redis
 
     local uri = ngx.var.uri
-    ngx.log(ngx.STDERR, "LUA Request ", uri)
+    ngx.log(ngx.DEBUG, "LUA Request ", uri)
 
 
     local redis = require "resty.redis"
@@ -9,6 +9,7 @@
     red:set_timeout(1000)
     local ok, err = red:connect("127.0.0.1", 6379)
     if not ok then
+        -- TODO set a status code
         ngx.say("Failed to connect to Redis: ", err)
         return
     end
@@ -20,6 +21,7 @@
     red:smembers("dead:zope")
     local ans, err = red:exec()
     if not ans then
+        -- TODO set a status code
         ngx.say("Lookup failed: ", err)
         return
     end
@@ -27,6 +29,7 @@
     -- Parse the result of the Redis lookup
     local backends = ans[1]
     if #backends == 0 then
+        -- TODO set a status code
         ngx.say("Backend not found for "..uri)
         return
     end
@@ -60,11 +63,12 @@
     -- Export variables
 
     if not backend then
+        -- TODO set a status code
         ngx.say("Backend not available")
         return
     end
 
-    ngx.log(ngx.STDERR, "LUA backend: ", backend)
+    ngx.log(ngx.DEBUG, "LUA backend: ", backend)
 
 
     ngx.var.backend = backend
