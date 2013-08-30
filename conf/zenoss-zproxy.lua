dd3aff5d3205c704492fd318283c9effd2976e0d
Chad Schellenger <cschellenger@zenoss.com>
2013-08-30 11:18:24 -0500

    Perform validate even when ZAuth token is provided by client

--- conf/zenoss-zproxy.lua
+++ conf/zenoss-zproxy.lua
@@ -20,6 +20,10 @@ local auth_token = req_headers['X-ZAuth-Token']
 if not auth_token then
    ngx.log(ngx.DEBUG, 'X-ZAuth-Token header was nil; checking ZAuthToken cookie')
    auth_token = ngx.var['cookie_ZAuthToken']
+   if auth_token then
+      -- If we got an auth token from a cookie, let's set an X-ZAuth-Token header
+      ngx.req.set_header('X-ZAuth-Token', auth_token)
+   end
    ngx.log(ngx.DEBUG, 'ZAuthToken cookie was ' .. (auth_token or 'nil'))
 else
    ngx.log(ngx.DEBUG, 'Found X-ZAuth-Token header')
@@ -36,26 +40,33 @@ end
 
 cjson = require "cjson"
 -- If we don't have a zauth token, we need to acquire one
-if not auth_token then
-   local lres = ngx.location.capture ('/zauth/api/login', { 
-                                         method = ngx.HTTP_GET,
-                                         body = nil,
-                                         share_all_vars = false,
-                                         copy_all_vars = false
-   })
-   if lres.status ~= ngx.HTTP_OK then
-      ngx.log(ngx.INFO, "Authentication failed")
-      ngx.status = lres.status
-      ngx.exit(ngx.HTTP_OK)
-      return
-   end
-   ngx.log(ngx.DEBUG, "Body: " .. lres.body)
-   -- Decode the login subrequest
-   token = cjson.decode(lres.body)
-   -- Add a ZAuth cookie to the response for future calls
-   ngx.header['Set-Cookie'] = {'ZAuthToken=' .. token['id'] .. '; path=/; Expires=' .. ngx.cookie_time(token['expires'])}
-   -- Set the ZAuth token in the proxy request
-   ngx.req.set_header('X-ZAuth-Token', token['id'])
-end
 
+local subrequest_url
+if auth_token then
+   subrequest_url = '/zauth/api/validate'
+else
+   subrequest_url = '/zauth/api/login'
+end
+ngx.log(ngx.DEBUG, 'Subrequest URL: ' .. subrequest_url)
+local lres = ngx.location.capture (subrequest_url, { 
+   method = ngx.HTTP_GET,
+   body = nil,
+   share_all_vars = false,
+   copy_all_vars = false
+})
+if lres.status ~= ngx.HTTP_OK then
+   ngx.log(ngx.INFO, "Authentication failed")
+   ngx.status = lres.status
+   ngx.exit(ngx.HTTP_OK)
+   return
+end
+ngx.log(ngx.DEBUG, "Body: " .. lres.body)
+-- Decode the subrequest
+token = cjson.decode(lres.body)
+-- Add a ZAuth cookie to the response for future calls
+if not auth_token then
+   ngx.header['Set-Cookie'] = {'ZAuthToken=' .. token['id'] .. '; path=/; Expires=' .. ngx.cookie_time(token['expires'])}
+end
+-- Set the ZAuth token in the proxy request
+ngx.req.set_header('X-ZAuth-Token', token['id'])
 rewrite_backend(uri, uri_prefix, backend)
