a3e959975a11de388b678591236ea3db6c9e1bb3
jplouis <jplouis@gmail.com>
2017-06-07 17:15:04 -0500

    add auth check to kibana urls

---
+++ src/conf/zenoss-require-auth.lua
@@ -0,0 +1,51 @@
+--[[----------------------------------------------------------------------------
+--
+-- Copyright (C) Zenoss, Inc. 2017, all rights reserved.
+--
+-- This content is made available according to terms specified in
+-- License.zenoss under the directory where your Zenoss product is installed.
+--
+--]]----------------------------------------------------------------------------
+
+local common = loadfile("conf/zproxy-common.lua")
+common()
+local req_headers = ngx.req.get_headers()
+local auth_token = extract_auth_token(req_headers)
+
+-- If we don't have a basic auth header or a zauth token, quit now
+if not req_headers['Authorization'] and not auth_token then
+    return exit_unauth()
+end
+
+cjson = require "cjson"
+-- Subrequest URL will be used for login or validate
+local subrequest_url
+if auth_token then
+    subrequest_url = '/zauth/api/validate'
+else
+    subrequest_url = '/zauth/api/login'
+end
+ngx.log(ngx.DEBUG, 'Subrequest URL: ' .. subrequest_url)
+-- Make a subrequest to login
+local substart = ngx.now()
+local lres = ngx.location.capture (subrequest_url, {
+    method = ngx.HTTP_GET,
+    body = nil,
+    share_all_vars = false,
+    copy_all_vars = false
+})
+ngx.var.subrequest_time = ngx.now() - substart
+-- If we got anything other than OK, bail out here.
+if lres.status ~= ngx.HTTP_OK then
+    return exit_authfailed(lres.status)
+end
+ngx.log(ngx.DEBUG, "Body: " .. lres.body)
+-- Decode the subrequest
+token = cjson.decode(lres.body)
+-- Add a ZAuth cookie to the response for future calls
+if not auth_token then
+    ngx.header['Set-Cookie'] = build_cookie(token)
+end
+-- Set the ZAuth token in the proxy request
+ngx.req.set_header('X-ZAuth-Token', token['id'])
+
